8094e82c1b4f73cb97038137c3142bcc0a371ad4
kmularise <kmula222@gmail.com>
2024-01-31 13:17:15 +0900

    refactor: FileFormat 함수 변경

--- src/main/java/project/server/spring/framework/util/FileFormat.java
+++ src/main/java/project/server/spring/framework/util/FileFormat.java
@@ -4,27 +4,27 @@ public enum FileFormat {
 	JPEG("image", "jpg"),
 	PNG("image", "png"),
 	CSS("stylesheet", "css"),
-	JS("script","js");
+	JS("script", "js");
 
 	private final String type;
-	private final String subType;
+	private final String extension;
 
-	FileFormat(String type, String subType) {
+	FileFormat(String type, String extension) {
 		this.type = type;
-		this.subType = subType;
+		this.extension = extension;
 	}
 
 	public String getType() {
 		return type;
 	}
 
-	public String getSubType() {
-		return subType;
+	public String getExtension() {
+		return extension;
 	}
 
 	public static FileFormat ofExtension(String extension) {
 		for (FileFormat format : FileFormat.values()) {
-			if (format.subType.equals(extension)) {
+			if (format.extension.equals(extension)) {
 				return format;
 			}
 		}
