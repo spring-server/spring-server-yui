35c2c621a338624ea6e4310e9674cf7ed9768154
kmularise <kmula222@gmail.com>
2024-03-14 11:34:24 +0900

    test: 로그 출력으로 수정

--- src/test/java/project/server/spring/framework/http/HttpHeadersTest.java
+++ src/test/java/project/server/spring/framework/http/HttpHeadersTest.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class HttpHeadersTest {
+	private Logger logger = LoggerFactory.getLogger(HttpHeadersTest.class);
+
 	@ParameterizedTest
 	@ValueSource(strings = {"Cookie: name=value; name2=value2; name3=value3"})
 	void parse(String headerValue) {
@@ -13,7 +17,7 @@ class HttpHeadersTest {
 		headers.parse(headerValue);
 		List<Cookie> cookies = headers.getCookies();
 		for (Cookie cookie : cookies) {
-			System.out.println(cookie.getName() + " " + cookie.getValue());
+			logger.info("cookie name:{}, cookie value:{}", cookie.getName(), cookie.getValue());
 		}
 	}
 }
