9cb136a4cddb149ef89b5c6ec0bf19c214f88415
kmularise <kmula222@gmail.com>
2024-03-21 22:36:23 +0900

    feat: application.yml 설정 파일 읽는 모듈 추가 - data source 설정 추가

---
+++ src/main/java/project/server/spring/framework/configuration/SpringConfig.java
@@ -0,0 +1,9 @@
+package project.server.spring.framework.configuration;
+
+public class SpringConfig {
+	private DataSourceConfig datasource;
+
+	public DataSourceConfig getDatasource() {
+		return datasource;
+	}
+}
